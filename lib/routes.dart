81d9a944c34e14069d57b5df711a4b35cc7a42aa
magnus.coder <kelimbth@gmail.com>
2025-05-13 20:09:11 +0300

    feat: added route name for onboarding screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'screens/forgot-password/forgot_password_screen.dart';
 import 'screens/login/login_screen.dart';
+import 'screens/onboarding/onboarding_screen.dart';
 import 'screens/otp/otp_screen.dart';
 import 'screens/signup/signup_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
+  OnboardingScreen.routeName: (context) => const OnboardingScreen(),
   LoginScreen.routeName: (context) => const LoginScreen(),
   ForgotPasswordScreen.routeName:(context) => const ForgotPasswordScreen(),
   SignupScreen.routeName: (context) => const SignupScreen(),
