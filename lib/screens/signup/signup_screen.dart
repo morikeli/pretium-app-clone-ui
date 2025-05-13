1036654803261f4d604ff18b9717e7751c71e787
magnus.coder <kelimbth@gmail.com>
2025-05-13 13:36:47 +0300

    feat: created a signup screen

---
+++ lib/screens/signup/signup_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'components/signup_screen_body.dart';
+
+class SignupScreen extends StatelessWidget {
+  static String routeName = '/signup';
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(automaticallyImplyLeading: true),
+      body: SignupScreenBody(),
+    );
+  }
+}
