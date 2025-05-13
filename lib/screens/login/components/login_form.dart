904c496685f813546e25c5e0ef6344e108ca7ab8
magnus.coder <kelimbth@gmail.com>
2025-05-13 20:18:18 +0300

    feat: extracted widgets to reduced nested widgets

--- lib/screens/login/components/login_form.dart
+++ lib/screens/login/components/login_form.dart
@@ -40,91 +40,109 @@ class _LoginFormState extends State<LoginForm> {
       key: widget.formKey,
       child: Column(
         children: [
-          CustomTextFormField(
-            controller: widget.emailController,
-            label: "Email",
-            icon: Icons.email_outlined,
-            keyboardType: TextInputType.emailAddress,
-            validator: (value) {
-              if ((value == null || value.isEmpty) &&
-                  !widget.formErrors.contains(kemailNullError)) {
-                setState(() {
-                  widget.formErrors.add(kemailNullError);
-                });
-              } else if (!emailValidatorRegex.hasMatch(value!) &&
-                  !widget.formErrors.contains(kInvalidEmailError)) {
-                setState(() {
-                  widget.formErrors.add(kInvalidEmailError);
-                });
-              }
-              return null;
-            },
-          ),
+          emailTextField(),
           const SizedBox(height: 20),
-          CustomTextFormField(
-              controller: widget.passwordController,
-              label: "Password",
-              icon: Icons.lock_outline,
-              obscureText: true,
-              validator: (value) {
-                if ((value == null || value.isEmpty) &&
-                    !widget.formErrors.contains(kPasswordNullError)) {
-                  setState(() {
-                    widget.formErrors.add(kPasswordNullError);
-                  });
-                } else if (value!.length < 8 &&
-                    !widget.formErrors.contains(kShortPasswordError)) {
-                  setState(() {
-                    widget.formErrors.add(kShortPasswordError);
-                  });
-                }
-                return null;
-              }),
-          SizedBox(height: 12.0),
-          Row(
-            children: [
-              Checkbox.adaptive(
-                  value: _rememberMe,
-                  activeColor: Colors.teal.shade900,
-                  onChanged: (value) {
-                    setState(() {
-                      _rememberMe = value!;
-                    });
-                  }),
-              const Text('Remember me'),
-              const Spacer(),
-              GestureDetector(
-                onTap: () => Navigator.pushNamed(context, '/forgot-password'),
-                child: Text(
-                  'Forgot password?',
-                  style: TextStyle(
-                    color: Colors.teal.shade900,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
-          ),
+          passwordTextField(),
+          const SizedBox(height: 12.0),
+
+          // "Remember Me" checkbox and "Forgot password" text
+          checkBoxandForgotPassword(context),
           const SizedBox(height: 8.0),
           FormErrorsWidget(errors: widget.formErrors),
           const SizedBox(height: 12.0),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {
-                if (widget.formKey.currentState!.validate()) {
-                  widget.formKey.currentState!.save();
-                  Navigator.pushNamed(context, OtpScreen.routeName);
-                }
-              },
-              child: const Text(
-                'Login',
-                style: TextStyle(fontSize: 18, color: Colors.white),
-              ),
-            ),
-          ),
+          loginButton(context),
         ],
       ),
+    );
+  }
+
+  SizedBox loginButton(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          if (widget.formKey.currentState!.validate()) {
+            widget.formKey.currentState!.save();
+            Navigator.pushNamed(context, OtpScreen.routeName);
+          }
+        },
+        child: const Text(
+          'Login',
+          style: TextStyle(fontSize: 18, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Row checkBoxandForgotPassword(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox.adaptive(
+            value: _rememberMe,
+            activeColor: Colors.teal.shade900,
+            onChanged: (value) {
+              setState(() {
+                _rememberMe = value!;
+              });
+            }),
+        const Text('Remember me'),
+        const Spacer(),
+        GestureDetector(
+          onTap: () => Navigator.pushNamed(context, '/forgot-password'),
+          child: Text(
+            'Forgot password?',
+            style: TextStyle(
+              color: Colors.teal.shade900,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  CustomTextFormField passwordTextField() {
+    return CustomTextFormField(
+        controller: widget.passwordController,
+        label: "Password",
+        icon: Icons.lock_outline,
+        obscureText: true,
+        validator: (value) {
+          if ((value == null || value.isEmpty) &&
+              !widget.formErrors.contains(kPasswordNullError)) {
+            setState(() {
+              widget.formErrors.add(kPasswordNullError);
+            });
+          } else if (value!.length < 8 &&
+              !widget.formErrors.contains(kShortPasswordError)) {
+            setState(() {
+              widget.formErrors.add(kShortPasswordError);
+            });
+          }
+          return null;
+        });
+  }
+
+  CustomTextFormField emailTextField() {
+    return CustomTextFormField(
+      controller: widget.emailController,
+      label: "Email",
+      icon: Icons.email_outlined,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if ((value == null || value.isEmpty) &&
+            !widget.formErrors.contains(kemailNullError)) {
+          setState(() {
+            widget.formErrors.add(kemailNullError);
+          });
+        } else if (!emailValidatorRegex.hasMatch(value!) &&
+            !widget.formErrors.contains(kInvalidEmailError)) {
+          setState(() {
+            widget.formErrors.add(kInvalidEmailError);
+          });
+        }
+        return null;
+      },
     );
   }
 }
