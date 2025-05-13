0b963d966abde151d00dda48f75cd0fad5d62567
magnus.coder <kelimbth@gmail.com>
2025-05-13 20:06:46 +0300

    feat: added elevated button theme data

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pretium_app_clone/theme/color_scheme.dart';
 import 'package:pretium_app_clone/theme/colors.dart';
+import 'package:pretium_app_clone/theme/theme_data/elevated_btn_theme.dart';
 
 import 'theme_data/input_decoration_theme_data.dart';
 import 'theme_data/text_theme_data.dart';
@@ -11,15 +12,16 @@ class PretiumAppTheme {
   static ThemeData lightTheme = ThemeData.light(useMaterial3: true).copyWith(
     colorScheme: PretiumAppColorScheme.colorSchemeLight,
     dividerColor: Colors.white,
+    elevatedButtonTheme: PretiumAppElevatedButtonTheme.elevatedButtonLightTheme,
     inputDecorationTheme: PretiumAppInputDecorationTheme.inputDecorationLightTheme,
     scaffoldBackgroundColor: kScaffoldBgLightColor,
     textTheme: PretiumAppTextTheme.lightTextTheme,
-
   );
 
   static ThemeData darkTheme = ThemeData.dark(useMaterial3: true).copyWith(
     colorScheme: PretiumAppColorScheme.colorSchemeDark,
     dividerColor: Colors.black,
+    elevatedButtonTheme: PretiumAppElevatedButtonTheme.elevatedButtonDarkTheme,
     inputDecorationTheme: PretiumAppInputDecorationTheme.inputDecorationDarkTheme,
     scaffoldBackgroundColor: kScaffoldBgDarkcolor,
     textTheme: PretiumAppTextTheme.darkTextTheme,
