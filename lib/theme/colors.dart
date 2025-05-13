936fd2c6c4c33601d95560789074ac3dfcb151d8
magnus.coder <kelimbth@gmail.com>
2025-05-13 17:38:29 +0300

    feat: app colors

---
+++ lib/theme/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFF004D40);
+const Color kSecondaryColor = Color(0xFF9E9E9E);
+const Color kScaffoldBgLightColor = Color(0xFFFFFFFF);
+const Color kScaffoldBgDarkcolor = Color(0xFF000000);
+const Color kContainerLightColor = Color(0xCCE0E0E0);
+const Color kContainerDarkColor = Color(0xFF004D40);
+const Color kIconDarkColor = Color(0xFF004D40);
+const Color kIconLightColor = Color(0xFFFFFFFF);
+const Color kTextLightColor = Color(0xFFFFFFFF);
+const Color kTextDarkColor = Color(0xFF000000);
+const Color kTextSecondaryColor = Color(0xFF8B8B8B);
+const Color kTextFormFieldLabelColor = Color(0xFF009688);
