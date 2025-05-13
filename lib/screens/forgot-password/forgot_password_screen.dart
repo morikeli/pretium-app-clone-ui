bb94a1e3527ac76608e51a734f8d3fa492dd3f48
magnus.coder <kelimbth@gmail.com>
2025-05-13 13:54:52 +0300

    feat: created a forgot password screen

---
+++ lib/screens/forgot-password/forgot_password_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:pretium_app_clone/screens/forgot-password/components/forgot_password_body.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  static String routeName = '/forgot-password';
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(automaticallyImplyLeading: true),
+      body: ForgotPasswordBody(),
+    );
+  }
+}
