7dd71de0700e343e07bd0e5a5db608f687da64cc
magnus.coder <kelimbth@gmail.com>
2025-05-13 12:42:45 +0300

    feat: created a custom form text field

---
+++ lib/common/custom_text_form_field.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatefulWidget {
+  final TextEditingController controller;
+  final String label;
+  final IconData icon;
+  final String? Function(String?) validator;
+  final bool obscureText;
+  final TextInputType keyboardType;
+
+  const CustomTextFormField({
+    super.key,
+    required this.controller,
+    required this.label,
+    required this.icon,
+    required this.validator,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+  });
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  late bool _obscure;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscure = widget.obscureText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      validator: widget.validator,
+      obscureText: _obscure,
+      keyboardType: widget.keyboardType,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.grey.shade50,
+            style: BorderStyle.none
+            // width: .5
+          ),
+          borderRadius: BorderRadius.circular(8.0)
+        ),
+        labelText: widget.label,
+        labelStyle: TextStyle(
+          color: Colors.teal
+        ),
+        prefixIcon: Icon(widget.icon),
+        suffixIcon: widget.obscureText
+            ? IconButton(
+                icon: Icon(
+                  _obscure ? Icons.visibility : Icons.visibility_off,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _obscure = !_obscure;
+                  });
+                },
+              )
+            : null,
+      ),
+    );
+  }
+}
