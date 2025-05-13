7e52a15fb0c85d4d07e7555a7e9d0454d074528c
magnus.coder <kelimbth@gmail.com>
2025-05-13 14:53:10 +0300

    feat: created an OTP screen

---
+++ lib/screens/otp/otp_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'components/otp_screen_body.dart';
+
+class OtpScreen extends StatelessWidget {
+  static String routeName = '/otp-screen';
+  const OtpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(automaticallyImplyLeading: true),
+      body: OtpScreenBody(),
+    );
+  }
+}
